050c2157b8ee5d140b13435deb510c305fe06652
Francis <hisconer@gmail.com>
2017-05-03 16:53:00 +0200

    minor refactor

--- styles.js
+++ styles.js
@@ -8,26 +8,26 @@ const alignment = {
   center: 2,
 };
 
-// Helper functions
-const hexToRGB =
-  (hex, alpha = 1) =>
-  ({
+// Read config file
+const config = JSON.parse(fs.readFileSync('fonts.json', 'utf8'));
+
+// Functions
+const hexToRGB = (hex, alpha = 1) => ({
     red: 1/(255/parseInt(hex.slice(1, 3), 16)),
     green: 1/(255/parseInt(hex.slice(3, 5), 16)),
     blue: 1/(255/parseInt(hex.slice(5, 7), 16)),
     alpha: alpha
   });
 
-// Config file
-const config = JSON.parse(fs.readFileSync('fonts.json', 'utf8'));
-
-// map the color
+// Parse color and alignment
 const parse = config.map(style => {
   style.color = hexToRGB(style.color);
   style.alignment = alignment[style.alignment];
   return style;
 });
 
+// JSONIFY
 const styleExportFile = JSON.stringify({ 'styles': parse }, null, 2);
 
+// Writeout!
 fs.writeFileSync('/Users/hisco/Desktop/_styles.json', styleExportFile);
